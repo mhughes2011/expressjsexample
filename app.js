534435a6cc4e4f40680ff52e5319d7c9beafb4b6
M Hughes <mhughes2011@gmail.com>
2021-05-27 17:07:59 -0700

    separated the layout even further

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is.", colors});
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
 });
 
 app.get('/hello', (req, res) => {
