51c0f76fed26412bd814c6025aa0d46e5af92916
M Hughes <mhughes2011@gmail.com>
2021-05-31 10:35:01 -0700

    added the redirects involving the cookie

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    //res.send('<h1>I love programming</h1>');
-    res.render('index');
+    const name = req.cookies.username;
+    if(name) {
+        res.render('index', {name: name});
+    } else {
+        res.redirect('/hello');
+    }
 });
 
 app.get('/cards', (req, res) => {
@@ -20,12 +24,17 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello', {name: req.cookies.username});
+    const name = req.cookies.username;
+    if(name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
 });
 
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username);
-    res.render('hello', {name: req.body.username});
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
