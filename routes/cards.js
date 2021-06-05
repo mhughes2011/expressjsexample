4856a5d0832aaadd526e7461c2b2b155096fdcc8
M Hughes <mhughes2011@gmail.com>
2021-06-04 20:51:23 -0700

    added routing via query strings

--- routes/cards.js
+++ routes/cards.js
@@ -9,10 +9,17 @@ const {cards} = data;
 
 //Implementing the :id in the route allows you to control the access of data via the url.  The :id specifically means a variable and whatever is put in there will be retrieved in the cards array.  If id=0 then it's cards[0], if id=apple then it's cards[apple] (which will come up with and error)
 router.get('/:id', (req, res) => {
-    res.render('card', { 
-        prompt: cards[req.params.id].question, 
-        hint: cards[req.params.id].hint
-    });
+    const {side} = req.query; //could also be const side = req.query.side
+    const {id} = req.params; //could also be const id = req.query.id
+    const text = cards[id][side];
+    const {hint} = cards[id];
+
+    const templateData = {text};
+
+    if(side === 'question') {
+        templateData.hint = hint;
+    }
+    res.render('card', templateData);
 });
 
 module.exports = router;
