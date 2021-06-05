078ffc4795345c76213a4033c621b50462a87d5f
M Hughes <mhughes2011@gmail.com>
2021-06-04 18:06:19 -0700

    minor edits to the cards route

--- routes/cards.js
+++ routes/cards.js
@@ -7,6 +7,7 @@ const {cards} = data;
 //The line above can be rewritten to the below code.  The above is ES6 format
 // const cards = data.cards;
 
+//Implementing the :id in the route allows you to control the access of data via the url.  The :id specifically means a variable and whatever is put in there will be retrieved in the cards array.  If id=0 then it's cards[0], if id=apple then it's cards[apple] (which will come up with and error)
 router.get('/:id', (req, res) => {
     res.render('card', { 
         prompt: cards[req.params.id].question, 
